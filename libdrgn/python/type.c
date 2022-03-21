7d2192cfc17545372f093c67834d7c57c75f0fb3
Kevin Svetlitski <svetlitski@fb.com>
2022-03-21 14:13:00 -0700

    drgn_oil_type_iterator now also returns the corresponding function address

--- libdrgn/python/type.c
+++ libdrgn/python/type.c
@@ -2363,7 +2363,8 @@ static void TypeIterator_dealloc(TypeIterator *self) {
 
 static PyObject *TypeIterator_next(TypeIterator *self) {
 	struct drgn_qualified_type *type;
-	struct drgn_error *err = self->is_oil_iterator ? drgn_oil_type_iterator_next(self->iterator, &type) : drgn_type_iterator_next(self->iterator, &type);
+	uintptr_t function_addr;
+	struct drgn_error *err = self->is_oil_iterator ? drgn_oil_type_iterator_next(self->iterator, &type, &function_addr) : drgn_type_iterator_next(self->iterator, &type);
 	if (err)
 		return set_drgn_error(err);
 	return type ? DrgnType_wrap(*type) : NULL;
