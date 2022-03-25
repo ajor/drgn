017367c7b0fc05c820d5502df20aef77e35ee741
Kevin Svetlitski <svetlitski@fb.com>
2022-03-25 12:06:25 -0700

    Prevent attempting to index an invalid namespace from crashing the process

--- libdrgn/dwarf_info.c
+++ libdrgn/dwarf_info.c
@@ -3407,6 +3407,9 @@ drgn_dwarf_info_update_index(struct drgn_dwarf_index_state *state)
 
 static struct drgn_error *index_namespace(struct drgn_namespace_dwarf_index *ns)
 {
+	if (!ns)
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+		                         "attempted to index NULL namespace");
 	if (ns->pending_dies.size == 0)
 		return NULL;
 
