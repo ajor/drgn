85ebbb808e99d6c73a779073493563d0962a400c
Kevin Svetlitski <svetlitski@fb.com>
2022-05-12 12:27:05 -0700

    Correctly handle inlined functions in `drgn_find_type_by_symbol_name`

--- libdrgn/dwarf_info.h
+++ libdrgn/dwarf_info.h
@@ -124,7 +124,13 @@ drgn_inlined_group_to_key(const struct drgn_inlined_group *group)
 
 DEFINE_HASH_MAP_TYPE(drgn_dwarf_type_map, const void *, struct drgn_dwarf_type)
 DEFINE_HASH_SET_TYPE(uintptr_set, uintptr_t)
-DEFINE_HASH_MAP_TYPE(drgn_inlined_map, uintptr_t, struct uintptr_set)
+
+struct drgn_inlined_group_info {
+  struct uintptr_set inlined_instances;
+  struct drgn_module *module;
+};
+
+DEFINE_HASH_MAP_TYPE(drgn_inlined_map, uintptr_t, struct drgn_inlined_group_info)
 DEFINE_HASH_SET_TYPE(drgn_inlined_group_set, struct drgn_inlined_group)
 
 /** DWARF debugging information for a program/@ref drgn_debug_info. */
@@ -331,6 +337,16 @@ struct drgn_error *
 drgn_type_from_dwarf(struct drgn_debug_info *dbinfo,
 		     struct drgn_module *module, Dwarf_Die *die,
 		     struct drgn_qualified_type *ret);
+
+struct drgn_error *
+drgn_dwarf_index_find_die(uintptr_t die_addr,
+			  struct drgn_module *module,
+			  Dwarf_Die *ret);
+
+struct drgn_error *
+drgn_abstract_origin_module(struct drgn_program *prog,
+			    uintptr_t abstract_origin,
+			    struct drgn_module **ret);
 
 /** @} */
 
