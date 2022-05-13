79a39bcc25417f2f2b6e42fdcbec958260fa6231
Alastair Robertson <alastairrob@hotmail.com>
2022-05-13 19:45:21 +0100

    Return multiple matching functions from `drgn_oil_type_iterator_next()`
    This lets us compile OIL with `-O0`.
    
    There are overloads of `CodegenHandler::getObjectSize()` with different
    arguments. When compiling with optimizations enabled, unused versions
    will not be present in the binary, so we would only match the overload
    being used. However, with optimizations turned off, all overloads will
    be present in the binary.
    
    Change the interface of `drgn_oil_type_iterator_next()` so that it can
    return all the present overloads of `getObjectSize()`.

--- libdrgn/python/type.c
+++ libdrgn/python/type.c
@@ -2358,8 +2358,12 @@ static void TypeIterator_dealloc(TypeIterator *self) {
 
 static PyObject *TypeIterator_next(TypeIterator *self) {
 	struct drgn_qualified_type *type;
-	uintptr_t function_addr;
-	struct drgn_error *err = self->is_oil_iterator ? drgn_oil_type_iterator_next(self->iterator, &type, &function_addr) : drgn_type_iterator_next(self->iterator, &type);
+	uintptr_t *function_addrs;
+	size_t function_addrs_len;
+	struct drgn_error *err = self->is_oil_iterator
+		? drgn_oil_type_iterator_next(self->iterator, &type, &function_addrs,
+				&function_addrs_len)
+		: drgn_type_iterator_next(self->iterator, &type);
 	if (err)
 		return set_drgn_error(err);
 	return type ? DrgnType_wrap(*type) : NULL;
